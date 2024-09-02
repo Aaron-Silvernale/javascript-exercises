ee597a38ad83a2559d2a3c3073f47a873bf18eb1
Aaron-Silvernale <135292960+Aaron-Silvernale@users.noreply.github.com>
2024-09-02 15:48:39 -0600

    successfully passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let str = word.split("");
+    let revStr = str.reverse();
+    let output = revStr.join("");
+    return output;
 };
 
 // Do not edit below this line
