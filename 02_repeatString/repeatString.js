e0ca3a5554c32f286311557b78256b3671525a78
Aaron-Silvernale <135292960+Aaron-Silvernale@users.noreply.github.com>
2024-09-02 15:32:40 -0600

    Made a working function that meets all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var output = "";
+    if (num < 0) {
+        return("ERROR");
+    } else
+    for (let i = 0; i < num; i++){
+        var test = output;
+        var output = test + string;
+    }
+    return(output);
 };
 
 // Do not edit below this line
